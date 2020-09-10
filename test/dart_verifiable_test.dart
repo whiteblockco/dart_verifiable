601cf2b889eabc5da6492b50f953e16ab013bf0a
dudco <dudco1129@naver.com>
2020-09-10 16:02:12 +0900

    Signer Test

--- test/dart_verifiable_test.dart
+++ test/dart_verifiable_test.dart
@@ -1,1 +1,19 @@
-void main() {}
+import 'dart:convert';
+
+import 'package:cryptography/cryptography.dart';
+import 'package:dart_verifiable/signer/ed25519/ed25519.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('signer', () async {
+    var keypair = await ed25519.newKeyPair();
+    Ed25519Singer signer = Ed25519Singer();
+    var data =
+        await signer.sign(utf8.encode("hello world"), keypair.privateKey);
+    print(data);
+
+    var verifed = await signer.verify(
+        utf8.encode("hello world"), data, keypair.publicKey);
+    print(verifed);
+  });
+}
